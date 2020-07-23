6d6e88fe544d9b2b9f5251bf4db03bb85c6b58cd
Adam K <gnqs@o2.pl>
2020-07-23 18:50:42 +0100

    complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import { IonApp } from '@ionic/react';
+import styled from 'styled-components';
+
+const IonAppStyled = styled(IonApp)`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  @media (min-width: 768px) {
+    display: grid;
+    grid-template-areas:
+      'header header'
+      'aside main';
+    grid-template-columns: 220px 1fr;
+    grid-template-rows: 4rem auto;
+  }
+`;
+
+const Header = styled.header`
+  grid-area: header;
+  background-color: #88d498;
+  color: #fbf7ef;
+  border: none;
+  padding: 0.5rem;
+`;
+
+const Aside = styled.aside`
+  grid-area: aside;
+  background-color: #c6dabf;
+  color: #fbf7ef;
+  border: none;
+  padding: 0.5rem;
+`;
+
+const Main = styled.main`
+  flex: 1;
+  grid-area: main;
+  background-color: #fbf7ef;
+  color: #1a936f;
+  border: none;
+  padding: 0.5rem;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <IonAppStyled>
+      <Header>
+        <p>header</p>
+      </Header>
+      <Aside>
+        <p>aside</p>
+      </Aside>
+      <Main>
+        <p>main</p>
+      </Main>
+    </IonAppStyled>
   );
 }
 
